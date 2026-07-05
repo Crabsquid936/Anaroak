212750349159ef76c30eba8b50b74f974a3826c8
FeydRautha3141 <gishhenry781@gmail.com>
2026-07-05 12:50:14 -0500

    Update message_encryption.py
    This will be for the messaging in Anaroak. Later, we will change this to JavaScript so it may work with the other assets. Also where are you? You didn't respond when I told you about how much my hair would sell for if I let it grow long enough.

--- message_encryption.py
+++ message_encryption.py
@@ -1,16 +1,16 @@
-char = input("Enter a character: ")
+char = input("Message: ")
 if len(char) == 1:
     unicode_value = ord(char)
     encrypted_numeric_value = 3 * unicode_value
     # Convert the encrypted numerical value back to a character
     encrypted_text = chr(encrypted_numeric_value)
-    print(f"The encrypted text for '{char}' is '{encrypted_text}' (Unicode value: {encrypted_numeric_value})")
+    print(encrypted_text)
 elif len(char) > 1:
     unicode_list = [ord(c) for c in char]
     # Correctly encrypt each value in the list
     encrypted_numeric_values = [3 * val for val in unicode_list]
     # Convert the list of encrypted numerical values back to a string
     encrypted_text = "".join([chr(val) for val in encrypted_numeric_values])
-    print(f"The encrypted text for '{char}' is '{encrypted_text}' (Numeric values: {encrypted_numeric_values})")
+    print(encrypted_text)
 else:
     print("No character entered.")
