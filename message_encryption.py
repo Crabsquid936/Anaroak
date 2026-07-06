8e21104dd55c467e79bf6e9e5a0f6290585d7a83
FeydRautha3141 <gishhenry781@gmail.com>
2026-07-06 16:39:08 -0500

    Refactor message encryption to use password input

--- message_encryption.py
+++ message_encryption.py
@@ -1,16 +1,10 @@
-char = input("Message: ")
-if len(char) == 1:
-    unicode_value = ord(char)
-    encrypted_numeric_value = 3 * unicode_value
-    # Convert the encrypted numerical value back to a character
-    encrypted_text = chr(encrypted_numeric_value)
-    print(encrypted_text)
-elif len(char) > 1:
+char = input("Password: ")
+if len(char) > 1:
     unicode_list = [ord(c) for c in char]
     # Correctly encrypt each value in the list
     encrypted_numeric_values = [3 * val for val in unicode_list]
     # Convert the list of encrypted numerical values back to a string
-    encrypted_text = "".join([chr(val) for val in encrypted_numeric_values])
-    print(encrypted_text)
+    password = "".join([chr(val) for val in encrypted_numeric_values])
+    password
 else:
-    print("No character entered.")
+    print("Invalid")
